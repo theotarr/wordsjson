ea35f8b4afaf8702eb064d7125b4825744fe7317
Jeff Tarr <jeff@tarr.com>
2022-02-09 15:30:34 -0500

    fix paths

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import os
 from flask import Flask, make_response
 import xmltodict
 
-
 app = Flask(__name__)
 @app.route('/api/v1/analysis/<string:word>', methods=['GET'])
 def analyze(word):
-    out = os.popen("cd /code/bin && ./wordsxml " + word)
+    out = os.popen("cd /code/wordjson/dist/bin && ./wordsxml " + word)
     return xmltodict.parse(out.read())
 
 if __name__ == '__main__':
