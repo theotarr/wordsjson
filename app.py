e1c6e0cab4fcedc7d384026276d1aa98e5a05a4a
Theodore Tarr <theodore@tarr.com>
2022-02-13 11:24:21 -0500

    standardize any amount of words

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 import os
-from flask import Flask
+import platform
 import xmltodict
-
+from flask import Flask, Response
+from util.standardize_output import standardize_lists
 app = Flask(__name__)
+
+
 @app.route('/api/v1/analysis/<string:word>', methods=['GET'])
 def analyze(word):
-    print(word)
-    out = os.popen("cd /code/wordsjson/dist/bin && ./wordsxml " + word)
-    return xmltodict.parse(out.read())
+    out = ""
+    
+    if platform.system() == 'Linux':
+        out = os.popen("cd /code/wordsjson/dist/bin && ./wordsxml " + word)
+
+    elif platform.system() == 'Windows':
+        out = os.popen("cd dist/WINNT_x86-gcc3 && wordsxml.exe " + word)
+
+    # parse response and return json
+    out = out.read()
+    resp = xmltodict.parse(out, dict_constructor=dict)
+    resp = standardize_lists(resp)
+    
+    # for some reason "jsonify" doesn't work here and the response has to be converted to a string and the content-type set manually
+    return Response(str(resp), mimetype='application/json')
+
+
 
 @app.route('/test')
 def test():
     return("Hello World")
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
