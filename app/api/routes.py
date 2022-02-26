fb0513d99b2afdf3391a143856f70e3f357934b3
Theo <theodore@tarr.com>
2022-02-26 12:48:43 -0500

    changed api route names and added an english-to-latin route

--- app/api/routes.py
+++ app/api/routes.py
@@ -9,10 +9,10 @@ from app.api.standardize_output import standardize_lists
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
 
-@api.route('/analysis/<string:word>', methods=['GET'])
+@api.route('/ln-to-en/<string:word>', methods=['GET'])
 def analyze(word):
     if not word:
-        return "Please provide a word to analyze.", 400
+        return "Please provide a Latin word to analyze.", 400
 
     # strip special characters (ie. accents, long marks, etc.)
     word = unidecode.unidecode(word)
@@ -33,4 +33,26 @@ def analyze(word):
     For some reason Flask's "jsonify" doesn't work here, so the response has 
     to be converted to a valid JSON string and then converted to a Response object.
     """
+    return Response(json.dumps(resp), mimetype='application/json'), 200
+
+
+@api.get('/en-to-ln/<string:word>')
+def en_to_ln(word):
+    if not word:
+        return "Please provide an English word to analyze.", 400
+
+    # strip special characters (ie. accents, long marks, etc.)
+    word = unidecode.unidecode(word)
+
+    out = ""
+    # platform specific intrusions
+    if platform.system() == 'Linux' or platform.system() == 'Darwin':
+        out = os.popen("cd /code/wordsjson/app/src && ./wordsxml ~e " + word)
+
+    elif platform.system() == 'Windows':
+        out = os.popen("cd app/src && wordsxml.exe ~e " + word)
+
+    # parse response and return json
+    resp = xmltodict.parse(out.read(), dict_constructor=dict)
+    resp = standardize_lists(resp)
     return Response(json.dumps(resp), mimetype='application/json'), 200
